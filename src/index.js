3494af046dda81c2ed59ec3a068ab62bb8588251
crusiris <cruzsirys@gmail.com>
2019-11-25 16:36:57 -0300

    Leyendo el archivo Markdown

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 //Desde este archivo debes exportar una función (mdLinks).
 
-const fs = require("fs");
+//Fs es un modulo de node que se encarga comunicarse con el sistema de archivos de la computadora
+const fs = require("fs"); //Lee todo el contenido de un archivo.
+
 const path = require("path");
 
-module.exports = () => {
+//Asincrono
+fs.readFile('./README.md', 'utf-8', (error, data) => {
 
-};
+    if (error) {
+        console.log(`Error ${error}`);
+    } else {
+        console.log(data);
+    }
+})
+
+//  module.exports = () => {
+
+
+// };
