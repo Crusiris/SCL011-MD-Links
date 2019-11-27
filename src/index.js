a14794a0bf0e2e0f3e219d6968fb2a115bee21c1
crusiris <cruzsirys@gmail.com>
2019-11-26 21:10:54 -0300

    Leyendo archivo Markdown, Utilizando promesas

--- src/index.js
+++ src/index.js
@@ -1,27 +1,52 @@
 "use strict"
 const fs = require("fs"); //Lee todo el contenido de un archivo.
-const path = require("path");
-const markdownLinkExtractor = require('markdown-link-extractor');
+const pathN = require("path");
+const markdownLinkExtractor = require('markdown-link-extractor'); //Libreria para extraer links
 
-
+//Funcion  de tipo promesa para leer archivo Markdown
 
 const getLink = () => {
-    //Obtengo de forma sincrona el archivo markdown con un metodo de node
-    let markdown = fs.readFileSync('./README.md', 'utf-8');
-    console.log(markdown);
-
-    // El archivo markdown es pasado como argumento al módulo extractor de links
-    let links = markdownLinkExtractor(markdown);
-
-    // Imprimo todos los links
-    links.forEach(function(link) {
-        console.log(link);
-    });
+    const data = new Promise((resolve, reject) => {
+        let path = process.argv[2];
+        path = pathN.resolve(path);
+        //Obtengo de forma sincrona el archivo markdown con un metodo de node
+        fs.readFile(path, 'utf-8', (err, result) => {
+            if (err) {
+                reject(`${err}`)
+            }
+            resolve(result);
+        })
+    })
+    return data;
 }
 
 
-getLink();
+getLink()
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.error(error.message)
+    });
 
-module.exports = {
-    getLink
-};
+
+
+// const getLink = () => {
+//     //Obtengo de forma sincrona el archivo markdown con un metodo de node
+//     let markdown = fs.readFileSync('./README.md', 'utf-8');
+//     console.log(markdown);
+
+//     // El archivo markdown es pasado como argumento al módulo extractor de links
+//     let links = markdownLinkExtractor(markdown);
+
+//     // Imprimo todos los links
+//     links.forEach(function(link) {
+//         console.log(link);
+//     });
+// }
+
+
+
+// module.exports = {
+//     getLink
+// };
